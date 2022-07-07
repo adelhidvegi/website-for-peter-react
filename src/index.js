d26ee77d9472eebb146930cf592be04e206f057f
Adél Hidvégi <adel.hidvegi@gmail.com>
2022-07-07 17:28:38 +0200

    add HashRouter

--- src/index.js
+++ src/index.js
@@ -8,9 +8,9 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/website-for-peter-react">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
