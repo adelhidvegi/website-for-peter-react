6fc957ccfd1db67dd856fbed80f6e121690756f8
Adél Hidvégi <adel.hidvegi@gmail.com>
2022-07-07 17:14:47 +0200

    change index.js

--- src/index.js
+++ src/index.js
@@ -8,7 +8,9 @@ import { BrowserRouter, HashRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/website-for-peter-react">
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
